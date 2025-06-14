7356f7ac11d0fbc6182ddb7082fb6a63e5cbeb17
rotemakiki <akiki@post.bgu.ac.il>
2025-06-14 13:26:45 +0300

    שינוי עמוד הוספת אקורדים
    יצירת תיבה ועוד
    יש עוד עבודה

---
+++ static/js/chord.js
@@ -0,0 +1,7 @@
+// English comment: Create a new chord object with chord name and visual width
+function createChord(chordName, width) {
+    return {
+        chord: chordName,
+        width: width
+    };
+}
